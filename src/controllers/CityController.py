1fd9d72f31235f579ffe409cb1bac5aabb5daecf
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-11-25 20:30:33 -0500

    Adding packages fastapi, hypercorn, pydantic

--- src/controllers/CityController.py
+++ src/controllers/CityController.py
@@ -12,3 +12,8 @@ def get_city(city_id: int):
 def create_city(city: City):
   db.append(city.dict())
   return db[-1]
+
+@app.delete('cities/{city_id}')
+def delete_city(city_id: int):
+  db.pop(city_id-1)
+  return {}
