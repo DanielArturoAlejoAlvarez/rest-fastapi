19780491051193cf50707f46dd58cccf5ea15d53
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-11-25 21:00:10 -0500

    Add worldtimeapi to our controller city

--- src/controllers/CityController.py
+++ src/controllers/CityController.py
@@ -1,14 +1,24 @@
 from app import *
+import requests
 
 @app.get('/cities')
 def get_cities():
-  return db
+  results=[]
+  for city in db:
+    r=requests.get(f'http://worldtimeapi.org/api/timezone/{city["name"]}/{city["timezone"]}')
+    current_time=r.json()['datetime']
+    results.append({
+      'name': city["name"],
+      'timezone': city["timezone"],
+      'current_time': current_time
+    })
+  return results
 
 @app.get('/cities/{city_id}')
 def get_city(city_id: int):
   return db[city_id-1]
 
-@app.post('cities')
+@app.post('/cities')
 def create_city(city: City):
   db.append(city.dict())
   return db[-1]
